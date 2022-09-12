f9e703ff70d219c6a7afb4b78e1d3769ea6cc01e
AytacSafarova <100721454+AytacSafarova@users.noreply.github.com>
2022-09-12 08:09:19 +0400

    Add files via upload

--- control/main.js
+++ control/main.js
@@ -1,33 +1,54 @@
-let password = document.querySelector(".password")
+let password = document.getElementById("password")
 let image = document.querySelector("img")
-let count = 3
-let arr = []
-password.addEventListener('keypress', changeblur)
-let num = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-function changeblur() {
-    let value = password.value
-    arr.push(value)
-    console.log(arr)
-    if (arr.length > 8) {
-        count = count + 1
-    }
-    arr.forEach(item => {
+let count =6
+console.log(password)
+let result=password.innerHTML
+console.log(result)
+console.log("salam")
 
-        if (num.includes(item)) {
-            count = count + 1
-            console.log(true)
-        }
-        else {
-            console.log(false)
-        }
-    })
-    if (count = 1) {
-        image.style.filter = "blur(10px)"
+let arr = []
+password.addEventListener('keyup', changeblur)
+function checkNumber(str,count) {
+    if(/\d/.test(str)) {
+      console.log("tr")
+console.log(count+1)
     }
-    if (count = 2) {
-        image.style.filter = "blur(9px)"
-    }
-    if (count = 3) {
-        image.style.filter = "blur(6px)"
-    }
+    console.log("checknumber")
+  }
+
+  function isUpper(str, count) {
+    if(/[A-Z]/.test(str)){
+      console.log(count+1)
+    };
+    
 }
+function containsSpecialChars(str) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if( specialChars.test(str)){
+     count+=1
+    };
+  }
+
+
+function changeblur() {
+  console.log(result)
+  //  checkNumber(value,count)
+  //  isUpper(value,count)
+  //  console.log(count)
+    // if (count = 1) {
+    //     image.style.filter = "blur(10px)"
+    //     console.log(1)
+    // }
+    // if (count = 2) {
+    //     image.style.filter = "blur(9px)"
+    //     console.log(1)
+
+    // }
+    // if (count = 3) {
+    //     image.style.filter = "blur(6px)"
+    //     console.log(1)
+
+    // }
+}
+
+console.log(result)
